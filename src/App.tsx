fcf3a0ee25c196919dda5aa7cc46b2ed4821bbbe
Tharun varshanth <tharunvar10@gmail.com>
2022-08-26 19:07:34 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AuthApi from "./app/utilities/api/auth";
+import Amplify, { Auth, API } from "aws-amplify";
+import AmazonCognitoIdentity from "amazon-cognito-identity-js";
+import AWS, { CognitoIdentityServiceProvider } from "aws-sdk";
+
+Amplify.configure({
+  aws_cognito_region: "ap-south-1",
+  aws_user_pools_id: "ap-south-1_aOUQu6r4O",
+  aws_user_pools_web_client_id: "244p26jusn0eum5i5f9l1gdjg8",
+});
 
 function App() {
+  const authController = useRef(new AuthApi());
+  let userSession: any;
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_ENV);
+    /*
+    Auth.currentSession().then((res) => {
+      let accessToken = res.getAccessToken();
+      let jwt = accessToken.getJwtToken();
+      // console.log(`myAccessToken: ${JSON.stringify(accessToken)}`);
+    });*/
+    return () => {};
+  }, []);
+
+  async function onFirstTimeSignUp() {
+    userSession = await authController.current.fistTimeSignUp();
+  }
+
+  function handleChangePassword() {
+    console.log(userSession);
+    if (userSession.challengeName === "NEW_PASSWORD_REQUIRED") {
+      authController.current.setFirstTimePassword(userSession);
+    }
+  }
+
+  function currentUser() {
+    authController.current.currentUser();
+  }
+
+  async function logout() {
+    await authController.current.signOut();
+  }
+  async function rememberDevice() {
+    authController.current.rememberDevice();
+  }
+  async function forgotDevice() {
+    authController.current.forgotDevice();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Tharun PWA App</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <button onClick={onFirstTimeSignUp}>First Signup</button>
+        <button onClick={handleChangePassword}>Set First Time Password</button>
+        <button onClick={currentUser}>Current User</button>
+        <button onClick={logout}>Logout</button>
+        <button onClick={logout}>Forgotpassword</button>
+        <button onClick={rememberDevice}>Remember</button>
+        <button onClick={forgotDevice}>Forgot Device</button>
       </header>
     </div>
   );
 }
-
+//npx serve -s build
 export default App;
---
+++ src/app/utilities/api/auth.tsx
@@ -0,0 +1,74 @@
+import { Amplify, Auth } from "aws-amplify";
+
+class AuthApi {
+  constructor() {}
+
+  async fistTimeSignUp() {
+    try {
+      const res = await Auth.signIn("+940768407950", "Tharun123");
+      console.log(res);
+      return res;
+    } catch (ex) {
+      console.log(ex);
+      return false;
+    }
+  }
+
+  async forgotPassword(username: string) {
+    try {
+      const res = await Auth.forgotPassword(username);
+      console.log(res);
+      return res;
+    } catch (ex) {
+      console.log(ex);
+      return false;
+    }
+  }
+
+  async setFirstTimePassword(user: any) {
+    try {
+      const res = await Auth.completeNewPassword(user, "Tharun123");
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async currentUser() {
+    try {
+      const res = await Auth.currentUserInfo();
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async rememberDevice() {
+    try {
+      const result = await Auth.rememberDevice();
+      console.log(result);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async forgotDevice() {
+    try {
+      const result = await Auth.forgetDevice();
+      console.log(result);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async signOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  }
+
+  async signIn() {}
+}
+export default AuthApi;
